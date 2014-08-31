b9d52e811f02c804064d6de88eeee6087b2d19e5
Travis Cross <tc@traviscross.com>
2014-08-31 22:36:39 +0000

    Follow cowbuilder convention of distros vs suites

--- cow-update.sh
+++ cow-update.sh
@@ -2,10 +2,10 @@
 set -e
 
 get_sources () {
-  local tgt_suite="$1"
-  while read type path suite components; do
+  local tgt_distro="$1"
+  while read type path distro components; do
     test "$type" = deb || continue
-    printf "$type $path $tgt_suite $components\n"
+    printf "$type $path $tgt_distro $components\n"
   done < /etc/apt/sources.list
 }
 
@@ -14,22 +14,22 @@ get_mirrors () {
 }
 
 cow_update () {
-  local suites="$1" archs="$2"
+  local distros="$1" archs="$2"
   local keyring="$(mktemp /tmp/keyringXXXXXXXX.asc)"
   apt-key exportall > $keyring
-  for suite in $suites; do
+  for distro in $distros; do
     for arch in $archs; do
-      printf "### Updating $suite/$arch\n\n">&2
-      local cow_img="/var/cache/pbuilder/base-$suite-$arch.cow"
+      printf "### Updating $distro/$arch\n\n">&2
+      local cow_img="/var/cache/pbuilder/base-$distro-$arch.cow"
       cow () {
-        printf "### Running cowbuilder $@ ($suite/$arch)...\n">&2
+        printf "### Running cowbuilder $@ ($distro/$arch)...\n">&2
         cowbuilder "$@" \
-          --distribution "$suite" \
+          --distribution "$distro" \
           --architecture "$arch" \
           --basepath "$cow_img" \
           --override-config \
           --keyring "$keyring" \
-          --othermirror "$(get_mirrors $suite)"
+          --othermirror "$(get_mirrors $distro)"
       }
       test -d "$cow_img" || cow --create
       cow --update
@@ -39,20 +39,20 @@ cow_update () {
 }
 
 usage () {
-  echo "$0 [-a <archs>] [-c <suites>]">&2
+  echo "$0 [-a <archs>] [-c <distros>]">&2
 }
 
 archs="amd64"
-suites="sid"
+distros="sid"
 test -z "$COWHELPER_ARCHS" || archs="$COWHELPER_ARCHS"
-test -z "$COWHELPER_SUITES" || suites="$COWHELPER_SUITES"
+test -z "$COWHELPER_DISTROS" || distros="$COWHELPER_DISTROS"
 while getopts "a:c:h" o; do
   case "$o" in
     h) usage; exit 0 ;;
     a) archs="$OPTARG" ;;
-    c) suites="$OPTARG" ;;
+    c) distros="$OPTARG" ;;
   esac
 done
 shift $(($OPTIND-1))
 
-cow_update "$suites" "$archs"
+cow_update "$distros" "$archs"
